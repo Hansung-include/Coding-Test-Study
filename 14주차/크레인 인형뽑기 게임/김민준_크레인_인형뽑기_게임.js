f18c23ecc1d0160d359afa42d4b56481da459a42
mj0107 <alswns1285@gmail.com>
2022-07-25 19:52:06 +0900

    크레인 인형뽑기 게임

--- 14주차/크레인 인형뽑기 게임/김민준_크레인_인형뽑기_게임.js
+++ 14주차/크레인 인형뽑기 게임/김민준_크레인_인형뽑기_게임.js
@@ -1,3 +1,4 @@
+// 게임 화면을 오른쪽 90도 rotate
 function rotate(board){
   let result = [];
   let len = board.length;
@@ -25,18 +26,25 @@ function solution(board, moves) {
   for(let i=0; i<moves.length; i++) {
       let row = moves[i] - 1;
       
+      // 뽑을 것이 없다면,
       if(rotate_board[row].length === 0) continue;
       
+      // 스택이 비어있다면 뽑은 것 그냥 push
       if(stack.length === 0) {
           stack.push(rotate_board[row].pop());
       }
+      // 스택이 비어있지 않다면,
       else {
           let last_idx = rotate_board[row].length-1;
+          // 스택의 맨위와 뽑은것이 같다면,
           if(stack[stack.length-1] === rotate_board[row][last_idx]) {
+              // 굳이 push할 필요없이 pop해서 스택의 맨위와 뽑은것 없애버린후,
+              // answer에 2를 더해줌
               rotate_board[row].pop();
               stack.pop(); 
               answer += 2;
           }
+          // 스택의 맨위와 뽑은것이 같지 않다면, push
           else {
               stack.push(rotate_board[row].pop());
           }
